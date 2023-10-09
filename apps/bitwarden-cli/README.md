33a8d58de116f8998de2413c3f78422706f1bd5e
Josh <jokajak@gmail.com>
2023-10-09 17:20:05 -0400

    feat: add bitwarden-cli

---
+++ apps/bitwarden-cli/README.md
@@ -0,0 +1,14 @@
+# bitwarden-cli
+
+Environment Variables
+
+## Required Environment Variables
+
+* `BW_CLIENTID`
+* `BW_CLIENTSECRET`
+* `BW_PASSWORD`
+
+```bash
+bw login --apikey
+bw unlock --passwordenv BW_PASSWORD
+```
---
+++ apps/bitwarden-cli/ci/latest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET "https://api.github.com/repos/bitwarden/clients/releases" | jq --raw-output 'map(select(.prerelease == false and .draft == false and (.tag_name | startswith("cli-")))) | sort_by(.tag_name) | .[-1].tag_name' 2>/dev/null)
+version="${version#*v}"
+printf "%s" "${version}"
