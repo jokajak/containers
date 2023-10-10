3ed15cf2c012f34ca0124b8948849d4f7c595d63
Josh <jokajak@gmail.com>
2023-10-10 06:32:42 -0400

    feat(bitwarden-cli): switch to apikey login

--- apps/bitwarden-cli/entrypoint.sh
+++ apps/bitwarden-cli/entrypoint.sh
@@ -4,9 +4,14 @@ set -e
 
 bw config server "${BW_HOST}"
 
-export BW_SESSION=$(bw login "${BW_USER}" --passwordenv BW_PASSWORD --raw)
+bw login --apikey --raw
+
+BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
+
+export BW_SESSION
 
 bw unlock --check
 
 echo 'Running `bw server` on port 8087'
+
 bw serve --hostname 0.0.0.0 #--disable-origin-protection
