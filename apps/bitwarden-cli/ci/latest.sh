4141fb0d2af048d12df8d22bfc83be7815e00447
Josh <jokajak@gmail.com>
2023-10-13 14:51:13 -0400

    fix(bitwarden-cli): install jq

--- apps/bitwarden-cli/ci/latest.sh
+++ apps/bitwarden-cli/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-patch=1
+patch=2
 version=$(curl -sX GET "https://api.github.com/repos/bitwarden/clients/releases" | jq --raw-output 'map(select(.prerelease == false and .draft == false and (.tag_name | startswith("cli-")))) | sort_by(.tag_name) | .[-1].tag_name' 2>/dev/null)
 version="${version#*v}"
 printf "%s" "${version}-${patch}"
