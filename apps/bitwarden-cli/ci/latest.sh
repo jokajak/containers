7bb556c344f33f1e8418a8f7c2005b3ffec0b0ed
Josh <jokajak@gmail.com>
2023-10-13 13:58:38 -0400

    feat(bitwarden-cli): switch to alpine based image

--- apps/bitwarden-cli/ci/latest.sh
+++ apps/bitwarden-cli/ci/latest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+patch=1
 version=$(curl -sX GET "https://api.github.com/repos/bitwarden/clients/releases" | jq --raw-output 'map(select(.prerelease == false and .draft == false and (.tag_name | startswith("cli-")))) | sort_by(.tag_name) | .[-1].tag_name' 2>/dev/null)
 version="${version#*v}"
-printf "%s" "${version}"
+printf "%s" "${version}-${patch}"
